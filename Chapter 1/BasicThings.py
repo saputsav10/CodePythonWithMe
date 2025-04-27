194c8d02ede3cad83bbe5fd4fb1ee089e798b8f9
Utsav Sapkota <saputsav10@gmail.com>
2025-04-27 15:56:35 +0545

    Practicing starting chapters

---
+++ Chapter 1/BasicThings.py
@@ -0,0 +1,26 @@
+# Printing the files inside a directory using Python
+
+# import os
+# directory_path='D:\Movie'
+# contents=os.listdir(directory_path)
+# for item in contents:
+#     print(item)
+
+#type conversion
+# a="13.2"
+# t1=type(a)
+# print(t1)
+# b=float(a)
+# t=type(b)
+# print(t)
+# print(b)
+
+#String Slice
+# name="Utsav"
+# nameshort=name[0:4]
+# print (nameshort)
+
+#Slicing with Skip value
+a="DattebayoNarutoUzumaki"
+b=a[2:15:3]
+print(b)
