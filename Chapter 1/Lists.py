cc04a4b5621b007720c1b139df0c56448632cd42
Utsav Sapkota <saputsav10@gmail.com>
2025-04-28 23:10:36 +0545

    Examples on Tuples and Lists

---
+++ Chapter 1/Lists.py
@@ -0,0 +1,28 @@
+# name=["Naruto","2", "1.5", "Sasuke", "100", "Dattebayo"]
+# print(name)
+# name.append("Kakashi")
+# print(name)
+# print(name.sort())
+# print(name.reverse())
+
+# l1=["Kathmandu", "Bhaktapur", 5, 10, 10.9, True, False, "Lalitpur"]
+# l1.pop(2)
+# print(l1)
+# l1.remove("Kathmandu")
+# print(l1)
+
+# a=("Naruto")
+# b=[1]
+# print(type(a))
+# print(type(b))
+# # print(a+b)
+
+fruits=["Orange", "Banana", "Grapes"]
+f1=(fruits.append("Pineapple"))
+print(f1)  #append doesn't apply on tuple, but can be applied on Lists, but doesn't return any value
+print(fruits)
+
+
+# fruits=("Orange", "Banana", "Grapes")
+# print(fruits.append("Pineapple"))
+# print(fruits)
