84088119e86dd0ca0bcd31dda1ea96e1cc2632ee
Utsav Sapkota <saputsav10@gmail.com>
2025-04-28 22:09:06 +0545

    Few practices on Strings

---
+++ Chapter 1/Strings.py
@@ -0,0 +1,7 @@
+# Name="Utsav Sapkota"
+# print(f"My name is {Name}")
+
+anime="Those who watch anime for the first time, they always become fan afterwards!"
+print(f"Quote on Anime {anime}")
+print(anime.find("a"))
+print(anime.replace("Those", "Those people"))
---
+++ Chapter 1/StringsQuestions.py
@@ -0,0 +1,11 @@
+# a=input("Enter your name: ")
+# b=input("Enter your address: ")
+# print ("Good Afternoon", a+ b)
+
+letter = "Yo, I am don. Tw hos kaun? "
+print(letter.replace("yo, ", "Hell!").replace("kaun", "damn"))
+print(letter.upper())
+print(letter.lower())
+print(letter.capitalize())
+print(letter.title())
+print(letter.uppder())
