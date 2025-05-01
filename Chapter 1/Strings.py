a97adfe7651ab9f7f817edf6df85f81c25005b18
Utsav Sapkota <saputsav10@gmail.com>
2025-05-01 23:36:28 +0545

    Minor changes

--- Chapter 1/Strings.py
+++ Chapter 1/Strings.py
@@ -21,10 +21,51 @@
 # print(a)
 # print(a1)
 
-a1=["Apple", "Banana", 5, 10, True, False]
-print(a1.append("Fruits"))
-a2=a1.append("Fruits")
-print(a2)
-print(type(a1))
+# a1=["Apple", "Banana", 5, 10, True, False]
+# # print(a1.append("Fruits"))
+# a2=a1.append("Fruits")
+# print(a2)
+# # print(a2)
+# print(type(a1))
+
+# a1.append("Fruits")
+# print(a1)
+
+# Fruits=["Apple", "Banana", "Pineapple", "Strawberry"]
+# Fruits.append("Watermelon")
+# # print(Fruits)
+
+# F1=Fruits.append("Watermelon")
+# print(F1)
+
+# Fruits=("Apple", "Banana", "Pineapple", "Strawberry")
+# Fruits2=("Mango",)
+# print(Fruits + Fruits2)
+
+# A1=[1, 10, 100, 150, 200, 250]
+# A2=A1.remove(100)
+# print(A1)
+
+# a = 2
+# b = 2.0
+# if a==b: 
+#     print ("The values are equal")
+# else:
+#     print ("The values are not equal")
+
+# if a===b: 
+
+a=" hello, Bipin  How are you? You seem a bit off today. Is there any problem with you my boy? come on, lets dance"
+# a1=print(a.strip())
+# print(a[2:39:3])
+print(a[:39])
+# print(a.find("Bipin"))
+print(a.find())
+
+
+
+
+
+
 
 
---
+++ Chapter 1/practice.py
@@ -0,0 +1,8 @@
+
+a1=(input("Enter the first number: "))
+a2=(input("Enter the second number: "))
+a3=a1+a2
+print(f"The remainder by dividing the first number from the second number is {a3}")
+print("I did it.")
+
+
