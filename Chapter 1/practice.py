bb9c743f6c40a3e0b9c4c766b4c5d5763f720195
Utsav Sapkota <saputsav10@gmail.com>
2025-05-02 23:40:58 +0545

    Lets roll bac

--- Chapter 1/practice.py
+++ Chapter 1/practice.py
@@ -4,5 +4,7 @@ a2=(input("Enter the second number: "))
 a3=a1+a2
 print(f"The remainder by dividing the first number from the second number is {a3}")
 print("I did it.")
+print(f"Let's roll back.")
+
 
 
